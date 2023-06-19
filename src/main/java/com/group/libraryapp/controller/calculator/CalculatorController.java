b1ef8d4a26fee08d1a463fc309c4fdce3ed0665d
Joong-main <76873740+Joong-main@users.noreply.github.com>
2023-06-20 07:44:36 +0900

    Initial commit

---
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -0,0 +1,22 @@
+package com.group.libraryapp.controller.calculator;
+
+import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CalculatorController {
+
+    @GetMapping("/add") // Get /add
+
+    public int addTwoNumbers(CalculatorAddRequest request){
+        return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply") // Post /multiply
+
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request){
+
+        return request.getNumber1() * request.getNumber2() ;
+    }
+}
