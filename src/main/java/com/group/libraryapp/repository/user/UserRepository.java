6d5669a32cd379eafa6cc3235f0e5aa84f0acfc1
leeji <leeji88460@gmail.com>
2023-07-04 10:45:24 +0900

    도서 반납기능 구현

--- src/main/java/com/group/libraryapp/repository/user/UserRepository.java
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -3,7 +3,10 @@ package com.group.libraryapp.repository.user;
 import com.group.libraryapp.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    User findByName(String name);
+
+    Optional<User> findByName(String name);
 }
--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -39,11 +39,7 @@ public class UserServiceV2 {
 
     @Transactional
     public void deleteUser(String name){
-        User user = userRepository.findByName(name);
-        if(user==null){
-            throw new IllegalArgumentException();
-        }
-
+        User user = userRepository.findByName(name).orElseThrow(IllegalArgumentException::new);
         userRepository.delete(user);
     }
 
